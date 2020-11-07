ad2a1adc7777d340777bfd62a74221d924e8e012
JRodDvlpr <Mialienproductions@gmail.com>
2020-11-07 13:08:46 -0500

    :rocket: exercise 1 completed:rocket:

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,22 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    ## Sorting to Dictionaries
+    ## Storing each weight as a key, value will be that weights index
+    w_dict = {}
     # Your code here
+    for i, k in enumerate(weights):
+        if k in w_dict:
+            w_dict[k].append(i)
+        else:
+            w_dict[k] = [i]
+
+    for i, k in enumerate(w_dict):
+        if limit-k in w_dict:
+            if w_dict[limit-k] is w_dict[k]:
+                if len(w_dict[k]) >= 2:
+                    return (w_dict[limit-k][1], w_dict[k][0])
+            else:
+                return (w_dict[limit-k][0], w_dict[k][0])
 
     return None
